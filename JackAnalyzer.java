bd6c8902e62215c6c7adc62cbac93dc27cb81f1f
Maarten Derks <made@freedom.nl>
2022-09-08 22:40:07 +0200

    Add main class and method

---
+++ JackAnalyzer.java
@@ -0,0 +1,18 @@
+/**
+ * This is the main program that drives the overall syntax analysis process, using the services of a
+ * <code>JackTokenizer</code> and a <code>CompilationEngine</code>. For each source <i>Xxx</i><code>.jack</code> file,
+ * the analyzer
+ * <ol>
+ *  <li>creates a <code>JackTokenizer</code> from the <i>Xxx</i><code>.jack</code> input file;</li>
+ *  <li>creates an output file named <i>Xxx</i><code>.xml</code>; and</li>
+ *  <li>uses the <code>JackTokenizer</code> and the <code>CompilationEngine</code> to parse the input file and write the parsed code to the output file.</li>
+ * </ol>
+ *
+ * @author Maarten Derks
+ */
+class JackAnalyzer {
+
+    public static void main(String[] args) {
+
+    }
+}
